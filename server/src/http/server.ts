a0f8ab48be5d3463800079f4c47ab46130d3732a
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-09-13 21:31:04 -0300

    finished backend and initialized frontend

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -8,13 +8,18 @@ import { createGoalRoute } from "./routes/create-goal";
 import { createGoalCompletionRoute } from "./routes/create-completion";
 import { getWeekPendingGoalsRoute } from "./routes/get-pending-goals";
 import { getWeekSummaryRoute } from "./routes/get-summary";
+import fastifyCors from "@fastify/cors";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.register(createGoalRoute)
+app.register(createGoalRoute);
 app.register(createGoalCompletionRoute);
 app.register(getWeekPendingGoalsRoute);
 app.register(getWeekSummaryRoute);
