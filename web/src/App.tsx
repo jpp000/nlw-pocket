953e38342a924a0849edc514a751233ccb0c09e5
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-09-13 22:30:34 -0300

    frontend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Dialog } from '@radix-ui/react-dialog'
+import { CreateGoal } from './components/create-goal'
+import { WeeklySummary } from './components/weekly-summary'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
+import { Loader2 } from 'lucide-react'
+import { EmptyGoals } from './components/empty-goals'
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+  const { data, isLoading } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+  })
+
+  if (isLoading || !data) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <Loader2 className="text-zinc-500 animate-spin size-10" />
+      </div>
+    )
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Dialog>
+      {data.summary.total > 0 ? (
+        <WeeklySummary summary={data.summary} />
+      ) : (
+        <EmptyGoals />
+      )}
+
+      <CreateGoal />
+    </Dialog>
   )
 }
-
-export default App
